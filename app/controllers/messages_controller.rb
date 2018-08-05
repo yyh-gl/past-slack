fd246a798cdd6f97bb23e9c0d21e66b11cf2a808
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-05 14:16:48 +0900

    だいたいの画面作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 
   def index
+    @channels = send_request(REQ_GET_PUBLIC_CHANNELS)
     @users = send_request(REQ_GET_ALL_USERS)
-    puts @users
   end
 
 end
