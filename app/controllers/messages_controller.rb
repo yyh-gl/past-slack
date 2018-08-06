3134548424b202534520cd66edf813845d6055f1
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-06 15:44:24 +0900

    リファクタ

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   protect_from_forgery prepend: true, with: :exception
   before_action :validate_params, only: :show
 
-
   def index
     @channels_name = get_channels_name
     @users_name = get_users_name
