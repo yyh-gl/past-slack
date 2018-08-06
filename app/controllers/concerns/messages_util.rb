214934fce7de89bb2b37207b839ee4c0f9005a49
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-06 09:14:19 +0900

    channelページ完成

--- app/controllers/concerns/messages_util.rb
+++ app/controllers/concerns/messages_util.rb
@@ -1,9 +1,20 @@
 module MessagesUtil
+  include UsersUtil
 
   def get_messages(keyword=nil)
     response = send_request(REQ_SEARCH_MESSAGES, channel_params)
     messages = response['messages']
+
+    users = get_users_name_with_avatar
     messages.each do |message|
+      if message['user'].present?
+        id = message['user'].to_s
+        message['user'] = users[id]['name']
+        message['avatar'] = users[id]['avatar']
+      else
+        message['user'] = 'unknown'
+        message['avatar'] = 'https://lh6.googleusercontent.com/-y-MY2satK-E/AAAAAAAAAAI/AAAAAAAAAJU/ER_hFddBheQ/photo.jpg'
+      end
       message['ts'] = Time.at(message['ts'].to_d)
     end
     messages.select! { |message| message['text'].include?(keyword) } if keyword.present?
--- app/controllers/concerns/users_util.rb
+++ app/controllers/concerns/users_util.rb
@@ -7,8 +7,32 @@ module UsersUtil
       response['members'].each do |user|
         users_name << user['name']
       end
-      users_name.sort!
+      users_name.sort
     end
+  end
+
+  def get_users_name_with_id
+    users_name = {}
+    response = send_request(REQ_GET_ALL_USERS)
+    if response['ok']
+      response['members'].each do |user|
+        users_name[user['id']] = user['name']
+      end
+      users_name.sort_by { |_, v| v }
+    end
+  end
+
+  def get_users_name_with_avatar
+    users = {}
+    response = send_request(REQ_GET_ALL_USERS)
+    if response['ok']
+      response['members'].each do |user|
+        users[user['id']] = {}
+        users[user['id']]['name'] = user['name']
+        users[user['id']]['avatar'] = user['profile']['image_32']
+      end
+    end
+    users
   end
 
 end
