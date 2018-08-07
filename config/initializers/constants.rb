586c7dea61c6efda4de1dc4537b5b9e1a9bf8226
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-07 14:16:23 +0900

    サーバ側のチャンネル一覧取得APIの変更に基づきフロントも修正

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 ## 定数定義
 # リクエストURI
 REQ_GET_ALL_USERS = '/users'.freeze
-REQ_GET_PUBLIC_CHANNELS = '/channels/db/public'.freeze
+REQ_GET_PUBLIC_CHANNELS = '/channels/public'.freeze
 REQ_GET_ALL_MESSAGES = '/messages'.freeze
 REQ_SEARCH_MESSAGES = '/messages/search'.freeze
